02b8a36ba6ee07a4ffb25e6e3f09e5815c374346
Nicholas Dover <dovern42@gmail.com>
2019-02-06 01:16:22 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
  # your code here!
+ i = 0
+ array = []
+  while i < collection.length
+    if (yield collection[i]) == true
+      array.push(collection[i])
+    end
+  end
+  array
 end
